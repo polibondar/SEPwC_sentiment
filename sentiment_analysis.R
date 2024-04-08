e6d7663da55ab9fc59bf2d754212241dfec5cfcd
jhill1 <jon.hill@york.ac.uk>
2024-04-08 19:12:53 +0100

    adding skeleton code

---
+++ sentiment_analysis.R
@@ -0,0 +1,52 @@
+suppressPackageStartupMessages({
+library(sentimentr)
+library(tidytext)
+library(lubridate)
+library(dplyr)
+library(tidyr)
+library(argparse)
+library(ggpubr)
+})
+
+load_data<-function(filename) {
+
+    return()
+}
+
+word_analysis<-function(toot_data, emotion) {
+
+    return()
+}
+
+sentiment_analysis<-function(toot_data) {
+
+    return()
+
+}
+
+main <- function(args) {
+
+}
+
+
+if(sys.nframe() == 0) {
+
+  # main program, called via Rscript
+  parser = ArgumentParser(
+                    prog="Sentiment Analysis",
+                    description="Analyse toots for word and sentence sentiments"
+                    )
+  parser$add_argument("filename",
+                    help="the file to read the toots from")
+  parser$add_argument("--emotion",
+                      default="anger",
+                      help="which emotion to search for")
+  parser$add_argument('-v', '--verbose',
+                    action='store_true',
+                    help="Print progress")
+  parser$add_argument('-p', '--plot',
+                    help="Plot something. Give the filename")
+  
+  args = parser$parse_args()  
+  main(args)
+}
