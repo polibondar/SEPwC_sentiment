7e24a8a000739671e13f521dbaaf1002e537501b
polibondar <polina365bondar@gmail.com>
2024-05-30 10:04:47 +0100

    working on tests 1 and 2

--- sentiment_analysis.R
+++ sentiment_analysis.R
@@ -9,23 +9,31 @@ library(ggpubr)
 })
 
 load_data<-function(filename) {
-  data <- read.csv(filename, sep = ",", header = TRUE, stringsAsFactors = FALSE, colClasses=c("id"="character"))
-  cleaned_data <- gsub("<[^>]+>", "", data$content)
+  data <- read.csv(filename, sep = ",", header = TRUE, stringsAsFactors = FALSE, colClasses=c("id"="character")) # preparing the data and giving the class to the "ID" column
+  cleaned_data <- gsub("<[^>]+>", "", data$content) # getting rid of html
   data$content <- cleaned_data
-  date_time <- ymd_hms(data$created_at)
+  date_time <- ymd_hms(data$created_at) # making sure the date format is year-month-date hour-minute-second
   data$created_at <- date_time
   data <- filter(data, language== "en")
     return(data)
 }
 
 word_analysis<-function(toot_data, emotion) {
-    nrc_joy <- get_sentiments("nrc") %>%
-      filter(sentiment == "joy")
-    
-   data %>%
-      filter(book == "data") %>%
+  tidy_data <- data() %>%
+    group_by(id) %>% # so we have each comment as a group
+    summarize()
+    mutate(            # adding columns
+      id = row_number())
+    ungroup() %>% 
+    unnest_tokens(word, text) #separating out the words
+  
+   nrc_joy <- get_sentiments("nrc") %>%
+    filter(sentiment == "joy")
+  data %>%
+      filter(data) %>%
       inner_join(nrc_joy) %>%
       count(word, sort = TRUE)
+  arrange (.data, desc(), .by_group = TRUE)
     return(data)
 }
 
@@ -61,3 +69,4 @@ if(sys.nframe() == 0) {
   args = parser$parse_args()  
   main(args)
 }
+
